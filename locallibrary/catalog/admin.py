0c67720aa3e090848dbea26e8b7f89384a1d8a96
tusharch12 <tusharch6577@gmail.com>
2024-06-27 18:09:58 +0530

    model added in admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Author, Book, Book_instance, Genre
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Book_instance)
+admin.site.register(Genre)
