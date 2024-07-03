b297120b286b9bb07b0257bce8aca32a96f6e596
tusharch12 <tusharch6577@gmail.com>
2024-07-03 17:23:48 +0530

    index view+added url+created template

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import urls
+from django.urls import path
 from . import views
 
-urlspatterns = [
-    
+urlpatterns = [
+    path('',views.index,name='index')
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Author,Book,Book_instance
 # Create your views here.
+
+def index(request):
+
+    num_of_books = Book.objects.all().count()
+    num_of_author = Author.objects.all().count()
+    num_of_book_instance = Book_instance.objects.all().count()
+
+    available_books = Book_instance.objects.filter(status = 'a').count()
+
+    context ={
+        'num_of_books': num_of_books,
+        'num_of_authors' : num_of_author,
+        'num_of_book_instances' : num_of_book_instance,
+        'available_books' : available_books
+    }
+
+    return render(request,'index.html',context)
